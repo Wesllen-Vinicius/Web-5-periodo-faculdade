6d78f20c1925f86a9d62478c6c136c09d17b9eac
Wes-debug <wesllenvinicius09@gmail.com>
2021-04-15 18:33:27 -0400

    primeiroCommit

---
+++ src/WebApp/Models/ConteudoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ConteudoViewModel
+    {
+        public string Titulo { get; set; }
+        public DateTime DataDoCadastro { get; set; }
+        public string Texto { get; set; }
+        public string LinkImagem { get; set; }
+    }
+}
---
+++ src/WebApp/Models/ProdutoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ProdutoViewModel
+    {
+        public string Tipo{ get; set; }
+        public string Valor { get; set; }
+        public string Descricao{ get; set; }
+    }
+}
